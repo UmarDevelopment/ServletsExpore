f77e4705f8929be26ebf13f141e6dff08faa7d3d
UmarDevelopment <fgsruss@gmail.com>
2024-05-12 22:30:22 +0300

    create example of proceeding parameter in get query

--- src/main/java/me/umar/firstweb/HelloServlet.java
+++ src/main/java/me/umar/firstweb/HelloServlet.java
@@ -6,15 +6,12 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        String message = "Hello "+request.getParameter("name");
+        System.out.println(message);
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
